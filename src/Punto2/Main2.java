a632013b698147ec069757a917d05354f5ccab9d
Martin <Martin@DESKTOP-TN2EHOQ>
2018-04-14 22:32:36 -0300

    clase main2

--- src/Punto2/Main2.java
+++ src/Punto2/Main2.java
@@ -10,5 +10,17 @@ package Punto2;
  * @author Martin
  */
 public class Main2 {
-    
+    public static void main(String[] args) {
+        
+        Rectangulo rectangulo = new Rectangulo();
+        Punto punto = new Punto();
+        punto.PuntoAsignarValor(3, 6);
+        rectangulo.ObtenerSegundoP(punto, 7);
+        rectangulo.ObtenerTercerP(punto, 7, 5);
+        rectangulo.ObtenerCuartoP(punto, 6);
+        double peri = rectangulo.ObtenerPerimetro(7, 6);
+        double sup = rectangulo.ObtenerSuperficie(7, 6);
+        System.out.println("El perimetro del rectangulo es : "+peri);
+        System.out.println("La superficie del rectangulo es : "+sup);
+    }
 }
