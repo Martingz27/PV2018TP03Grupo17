4a3522120ad4ff0e8486bd070f34e8a8caf0250d
Martin <Martin@DESKTOP-TN2EHOQ>
2018-04-14 22:31:31 -0300

    clase punto

--- src/Punto2/Punto.java
+++ src/Punto2/Punto.java
@@ -10,5 +10,25 @@ package Punto2;
  * @author Martin
  */
 public class Punto {
+    private double x,y;
+   
+   public void PuntoAsignarValor(double x, double y){
+       this.x = x;
+       this.y = y;
+   }
+    public double getX() {
+        return x;
+    }
     
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
 }
